e20c1957787282f782449e1ca27a38df62b54ba2
khushi sharma <khushisharam2332@gmail.com>
2022-09-14 11:29:26 +0530

    add user role

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -5,9 +5,9 @@ class Users::RegistrationsController < Devise::RegistrationsController
   before_action :configure_account_update_params, only: [:update]
 
   # GET /resource/sign_up
-  # def new
-  #   super
-  # end
+  def new
+    super
+  end
 
   # POST /resource
   def create
@@ -42,7 +42,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_sign_up_params
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :contact_number, :company, :url, :image, :image_cache])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :contact_number, :company, :role, :url, :image, :image_cache])
   end
 
   # If you have extra params to permit, append them to the sanitizer.
